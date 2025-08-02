1d49898fef065b1bf2741b075ee819334ebab408
CSubham <csubham.dev@gmail.com>
2025-08-02 18:29:48 +0530

    ghost commit

--- main.c
+++ main.c
@@ -8,23 +8,36 @@
 
 // Board and cell size
 #define WINDOW_SIZE 960
-#define CELL_SIZE   120
+#define CELL_SIZE 120
 
 // Pieces
-enum PieceType {
-    WP, WR, WN, WB, WQ, WK, // White pieces
-    BP, BR, BN, BB, BQ, BK, // Black pieces
+enum PieceType
+{
+    WP,
+    WR,
+    WN,
+    WB,
+    WQ,
+    WK, // White pieces
+    BP,
+    BR,
+    BN,
+    BB,
+    BQ,
+    BK, // Black pieces
     NONE
 };
 
 // Board coordinate
-struct coordinate {
+struct coordinate
+{
     int x;
     int y;
 };
 
 // Piece structure
-struct piece {
+struct piece
+{
     struct coordinate coord;
     enum PieceType recog;
     int moved;
@@ -37,34 +50,44 @@ struct piece {
 struct piece board[8][8];
 
 // Piece textures
-SDL_Texture* pieceTextures[12];
+SDL_Texture *pieceTextures[12];
 
 int init();
 int print_board(struct piece board[8][8]);
-SDL_Texture* loadTexture(SDL_Renderer* renderer, const char* path);
-void drawPiece(SDL_Renderer* renderer, SDL_Texture* texture, int row, int col);
+SDL_Texture *loadTexture(SDL_Renderer *renderer, const char *path);
+void drawPiece(SDL_Renderer *renderer, SDL_Texture *texture, int row, int col);
 int getTextureIndex(struct piece p);
 
-// Init the board with 
-int init(){
+// Init the board with
+int init()
+{
+    // initilisation with empty first
+    for (int row = 0; row < 8; row++)
+    {
+        for (int col = 0; col < 8; col++)
+        {
+            board[row][col] = (struct piece){{row, col}, NONE, 0, 0, 0, 0};
+        }
+    }
 
     // Place pawns for both colors
-    for (int i = 0; i < 8; i++) {
-        struct piece wpawn = {{-1, -1}, WP, 0, 0, 0, 1};
+    for (int i = 0; i < 8; i++)
+    {
+        struct piece wpawn = {{6, i}, WP, 0, 0, 0, 1};
         board[6][i] = wpawn;
-        struct piece bpawn = {{-1, -1}, BP, 0, 0, 0, 0};
+        struct piece bpawn = {{1, i}, BP, 0, 0, 0, 0};
         board[1][i] = bpawn;
     }
 
     // White pieces (bottom row)
-    struct piece wrook1   = {{7, 0}, WR,   0, 0, 0, 1};
-    struct piece wknight1 = {{7, 1}, WK, 0, 0, 0, 1};
+    struct piece wrook1 = {{7, 0}, WR, 0, 0, 0, 1};
+    struct piece wknight1 = {{7, 1}, WN, 0, 0, 0, 1};
     struct piece wbishop1 = {{7, 2}, WB, 0, 0, 0, 1};
-    struct piece wqueen   = {{7, 3}, WQ,  0, 0, 0, 1};
-    struct piece wking    = {{7, 4}, WK,   0, 0, 0, 1};
+    struct piece wqueen = {{7, 3}, WQ, 0, 0, 0, 1};
+    struct piece wking = {{7, 4}, WK, 0, 0, 0, 1};
     struct piece wbishop2 = {{7, 5}, WB, 0, 0, 0, 1};
-    struct piece wknight2 = {{7, 6}, WK, 0, 0, 0, 1};
-    struct piece wrook2   = {{7, 7}, WR,   0, 0, 0, 1};
+    struct piece wknight2 = {{7, 6}, WN, 0, 0, 0, 1};
+    struct piece wrook2 = {{7, 7}, WR, 0, 0, 0, 1};
 
     // Place white pieces
     board[7][0] = wrook1;
@@ -77,14 +100,14 @@ int init(){
     board[7][7] = wrook2;
 
     // Black pieces (top row)
-    struct piece brook1   = {{0, 0}, BR,   0, 0, 0, 0};
-    struct piece bknight1 = {{0, 1}, BK, 0, 0, 0, 0};
+    struct piece brook1 = {{0, 0}, BR, 0, 0, 0, 0};
+    struct piece bknight1 = {{0, 1}, BN, 0, 0, 0, 0};
     struct piece bbishop1 = {{0, 2}, BB, 0, 0, 0, 0};
-    struct piece bking    = {{0, 3}, BK,   0, 0, 0, 0};
-    struct piece bqueen   = {{0, 4}, BQ,  0, 0, 0, 0};
+    struct piece bking = {{0, 3}, BK, 0, 0, 0, 0};
+    struct piece bqueen = {{0, 4}, BQ, 0, 0, 0, 0};
     struct piece bbishop2 = {{0, 5}, BB, 0, 0, 0, 0};
-    struct piece bknight2 = {{0, 6}, BK, 0, 0, 0, 0};
-    struct piece brook2   = {{0, 7}, BR, 0, 0, 0, 0};
+    struct piece bknight2 = {{0, 6}, BN, 0, 0, 0, 0};
+    struct piece brook2 = {{0, 7}, BR, 0, 0, 0, 0};
 
     // Place black pieces
     board[0][0] = brook1;
@@ -100,15 +123,21 @@ int init(){
 }
 
 // Print the board to the console (for debugging)
-int print_board(struct piece board[8][8]) {
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-            if (board[i][j].recog != 0) {
+int print_board(struct piece board[8][8])
+{
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (board[i][j].recog != 0)
+            {
                 if (board[i][j].color == 1)
                     printf("%c ", toupper(board[i][j].recog));
                 else
                     printf("%c ", board[i][j].recog);
-            } else {
+            }
+            else
+            {
                 printf("_ ");
             }
         }
@@ -117,8 +146,9 @@ int print_board(struct piece board[8][8]) {
     return 0;
 }
 
-//Load piece images png file
-void loadImagesPNG(SDL_Renderer* renderer){
+// Load piece images png file
+void loadImagesPNG(SDL_Renderer *renderer)
+{
     // Load piece images (PNG files)
     pieceTextures[WP] = loadTexture(renderer, "images/Chess_plt45.png");
     pieceTextures[WR] = loadTexture(renderer, "images/Chess_rlt45.png");
@@ -133,47 +163,48 @@ void loadImagesPNG(SDL_Renderer* renderer){
     pieceTextures[BQ] = loadTexture(renderer, "images/Chess_qdt45.png");
     pieceTextures[BK] = loadTexture(renderer, "images/Chess_kdt45.png");
 }
+
 // Load image file into SDL_Texture
-SDL_Texture* loadTexture(SDL_Renderer* renderer, const char* path) {
-    SDL_Surface* surface = IMG_Load(path);
-    if (!surface) {
+SDL_Texture *loadTexture(SDL_Renderer *renderer, const char *path)
+{
+    SDL_Surface *surface = IMG_Load(path);
+    if (!surface)
+    {
         printf("IMG_Load failed: %s\n", IMG_GetError());
         return NULL;
     }
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
     SDL_FreeSurface(surface);
     return texture;
 }
 
 // Draw a piece at a given board coordinate
-void drawPiece(SDL_Renderer* renderer, SDL_Texture* texture, int row, int col) {
+void drawPiece(SDL_Renderer *renderer, SDL_Texture *texture, int row, int col)
+{
     SDL_Rect dst = {
         col * CELL_SIZE,
         row * CELL_SIZE,
         CELL_SIZE,
-        CELL_SIZE
-    };
+        CELL_SIZE};
     SDL_RenderCopy(renderer, texture, NULL, &dst);
 }
 
 // Map struct piece to enum PieceType index for texture lookup
-int getTextureIndex(struct piece p) {
-    if (p.recog == 0) return -1;
-    bool isWhite = p.color == 1;
-    switch (tolower(p.recog)) {
-        case 'p': return isWhite ? WP : BP;
-        case 'r': return isWhite ? WR : BR;
-        case 'n': return isWhite ? WN : BN;
-        case 'b': return isWhite ? WB : BB;
-        case 'q': return isWhite ? WQ : BQ;
-        case 'k': return isWhite ? WK : BK;
+
+int getTextureIndex(struct piece p)
+{
+    if (p.recog >= WP && p.recog <= BK)
+    {
+        return p.recog;
     }
     return -1;
 }
 
-//Cleanup
-void cleanup(SDL_Renderer* renderer, SDL_Window* window){
-    for (int i = 0; i < 12; i++) {
+// Cleanup
+void cleanup(SDL_Renderer *renderer, SDL_Window *window)
+{
+    for (int i = 0; i < 12; i++)
+    {
         if (pieceTextures[i])
             SDL_DestroyTexture(pieceTextures[i]);
     }
@@ -181,84 +212,82 @@ void cleanup(SDL_Renderer* renderer, SDL_Window* window){
     SDL_DestroyWindow(window);
     IMG_Quit();
     SDL_Quit();
-
 }
 
-//Draw chessboard squares and pieces
-void drawChessBoard(SDL_Renderer* renderer){
-    for (int row = 0; row < 8; row++) {
-            for (int col = 0; col < 8; col++) {
-                // Draw board square
-                SDL_Rect square = {
-                    col * CELL_SIZE,
-                    row * CELL_SIZE,
-                    CELL_SIZE,
-                    CELL_SIZE
-                };
-                if ((row + col) % 2 == 0)
-                    SDL_SetRenderDrawColor(renderer, 240, 217, 181, 255); 
-                else
-                    SDL_SetRenderDrawColor(renderer, 181, 136, 99, 255);  
-                SDL_RenderFillRect(renderer, &square);
+// Draw chessboard squares and pieces
+void drawChessBoard(SDL_Renderer *renderer)
+{
+    for (int row = 0; row < 8; row++)
+    {
+        for (int col = 0; col < 8; col++)
+        {
+            // Draw board square
+            SDL_Rect square = {
+                col * CELL_SIZE,
+                row * CELL_SIZE,
+                CELL_SIZE,
+                CELL_SIZE};
+            if ((row + col) % 2 == 0)
+                SDL_SetRenderDrawColor(renderer, 240, 217, 181, 255);
+            else
+                SDL_SetRenderDrawColor(renderer, 181, 136, 99, 255);
+            SDL_RenderFillRect(renderer, &square);
 
-                // Draw piece if present
-                int idx = getTextureIndex(board[row][col]);
-                if (idx != -1 && pieceTextures[idx]) {
-                    drawPiece(renderer, pieceTextures[idx], row, col);
-                }
+            // Draw piece if present
+            int idx = getTextureIndex(board[row][col]);
+            if (idx != -1 && pieceTextures[idx])
+            {
+                drawPiece(renderer, pieceTextures[idx], row, col);
             }
         }
-
+    }
 }
 
 // Main code
-int main() {
+int main()
+{
     // Initialize SDL and SDL_image
     SDL_Init(SDL_INIT_VIDEO);
     IMG_Init(IMG_INIT_PNG);
 
     // Create window and renderer
-    SDL_Window* window = SDL_CreateWindow("Chessboard",
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        WINDOW_SIZE, WINDOW_SIZE, 0);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+    SDL_Window *window = SDL_CreateWindow("Chessboard",
+                                          SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                          WINDOW_SIZE, WINDOW_SIZE, 0);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
 
     // Initialize the chess board
     init();
 
-    //load png images
-    loadImagesPNG(&renderer);
-    
+    // load png images
+    loadImagesPNG(renderer);
 
     // Main Loop
     bool running = true;
     SDL_Event event;
 
-    while (running) {
-        while (SDL_PollEvent(&event)) {
+    while (running)
+    {
+        while (SDL_PollEvent(&event))
+        {
             if (event.type == SDL_QUIT)
                 running = false;
         }
-        //Chess Logic code
+        // Chess Logic code
 
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); 
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         SDL_RenderClear(renderer);
 
         // Draw chessboard squares and pieces
-        drawChessBoard(&renderer);
-        
+        drawChessBoard(renderer);
+
         // Display window
         SDL_RenderPresent(renderer);
-        SDL_Delay(16); 
+        SDL_Delay(16);
     }
 
     // Exit
-    drawChessBoard(&renderer);
-    
+    cleanup(renderer, window);
 
     return 0;
 }
-
-
-
-
